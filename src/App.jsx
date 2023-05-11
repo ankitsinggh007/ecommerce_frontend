1646719d870b4c05cf3b16d0dcab9a738064ff2e
ankitsinggh007 <as3824115@gmail.com>
2023-05-12 02:21:53 +0530

    added user login and sign up data collection function

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './Pages/Home'
-import Login from './Pages/LoginSignUp'
-import Account from './Pages/Account'
-import { UpdatePassowrd, UpdateProfile } from './Pages/updateUserProfile'
-import Cart from './Pages/Cart'
-import Product from './Pages/Product'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Login from "./Pages/LoginSignUp";
+import Account from "./Pages/Account";
+import { UpdatePassowrd, UpdateProfile } from "./Pages/updateUserProfile";
+import Cart from "./Pages/Cart";
+import Product from "./Pages/Product";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://muddy-pike-cardigan.cyclic.app";
+
 function App() {
-
   return (
-<Routes>      
-      <Route path='/' element={<Home/>}>
-      <Route path='/' element={<Product category='featured'/>}/>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route path="/" element={<Product category="featured" />} />
 
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/account' element={<Account/>}/>
-        <Route path='/update_account' element={<UpdateProfile/>}/>
-        <Route path='/update_password' element={<UpdatePassowrd/>}/>
-        <Route path='/cart' element={<Cart/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/update_account" element={<UpdateProfile />} />
+        <Route path="/update_password" element={<UpdatePassowrd />} />
+        <Route path="/cart" element={<Cart />} />
       </Route>
-    
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/LoginSignUp Componenet.jsx
+++ src/component/LoginSignUp Componenet.jsx
@@ -6,11 +6,14 @@ import {
   FormErrorMessage,
   Input,
 } from "@chakra-ui/react";
-import { Box,Text,Flex } from "@chakra-ui/react"; 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Box, Text, Flex } from "@chakra-ui/react";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
+import { useState } from "react";
 
- export function SignUp() {
+export function SignUp() {
+  const [ConfirmPasswordNotMatched, setConfirmPasswordNotMatched] =
+    useState(false);
 
   const handleRegisterForm = async (e) => {
     e.preventDefault();
@@ -18,118 +21,145 @@ import 'react-toastify/dist/ReactToastify.css';
       name: e.target[0].value,
       email: e.target[1].value,
       password: e.target[2].value,
-      conformPassword: e.target[3].value,
+      confirmPassword: e.target[3].value,
     };
+    console.log(form, "form");
+    if (form.password !== form.confirmPassword) {
+      setConfirmPasswordNotMatched(true);
+      return;
+    }
+    setConfirmPasswordNotMatched(false);
 
-
-    if(form.password!==form.conformPassword) return alert("password is not matched");
-    console.log("submitted");
-  }
-  const isError = false;
-
+    const userDetails = {
+      name: form.name,
+      email: form.email,
+      password: form.password,
+    };
+    console.log(userDetails, "userDetails");
+  };
   return (
-   <Flex flexDirection='row'>
-    
-    <Flex flexDirection='column' rowGap='10px' w='50%' p='10px' bg='royalBlue'>
-    <Text color='white' mt='20px' fontSize='1.2rem'>Looks like you're new here</Text>
-    <Text color='white' fontSize='1.2rem'>sign up with your email to get started shopping</Text>
+    <Flex flexDirection="row">
+      <Flex
+        flexDirection="column"
+        rowGap="10px"
+        w="50%"
+        p="10px"
+        bg="royalBlue"
+      >
+        <Text color="white" mt="20px" fontSize="1.2rem">
+          Looks like you're new here
+        </Text>
+        <Text color="white" fontSize="1.2rem">
+          sign up with your email to get started shopping
+        </Text>
+      </Flex>
+      <form onSubmit={handleRegisterForm}>
+        <Text
+          color="royalBlue"
+          fontWeight="medium"
+          fontSize="1.6rem"
+          mb="30px"
+          textAlign="center"
+        >
+          Register
+        </Text>
 
-      
+        <FormControl isRequired>
+          <FormLabel>Name</FormLabel>
+          <Input placeholder="Name" />
+        </FormControl>
+
+        <FormControl isRequired>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" />
+        </FormControl>
+
+        <FormControl isRequired>
+          <FormLabel>password</FormLabel>
+          <Input placeholder="password" />
+        </FormControl>
+
+        <FormControl isRequired isInvalid={ConfirmPasswordNotMatched}>
+          <FormLabel>conform password</FormLabel>
+          <Input placeholder="conform password" />
+          <FormErrorMessage>password is not matched</FormErrorMessage>
+        </FormControl>
+        <Button
+          align="center"
+          mt={4}
+          bg="#fb641b"
+          color="white"
+          // isLoading={props.isSubmitting}
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
+      <ToastContainer />
     </Flex>
-     <form onSubmit={handleRegisterForm}>
-    <Text color='royalBlue' fontWeight='medium' fontSize='1.6rem' mb='30px' textAlign='center'>Register</Text>
-
-      <FormControl isRequired>
-        <FormLabel>Name</FormLabel>
-        <Input placeholder="Name" />
-      </FormControl>
-
-      <FormControl isRequired isInvalid={isError}>
-        <FormLabel>Email</FormLabel>
-        <Input type="email" />
-        {!isError ? (
-          <FormHelperText>
-            Enter the email you'd like to receive the newsletter on.
-          </FormHelperText>
-        ) : (
-          <FormErrorMessage>Email is required.</FormErrorMessage>
-        )}
-      </FormControl>
-
-      <FormControl isRequired>
-        <FormLabel>password</FormLabel>
-        <Input placeholder="password" />
-      </FormControl>
-
-      <FormControl isRequired>
-        <FormLabel>conform password</FormLabel>
-        <Input placeholder="conform password" />
-      </FormControl>
-      <Button
-      align='center'
-            mt={4}
-            bg='#fb641b'
-            color='white'
-            // isLoading={props.isSubmitting}
-            type='submit'
-          >
-            Submit
-          </Button>
-    </form>
-   </Flex>
   );
 }
 export function Login() {
-
   const handleRegisterForm = async (e) => {
     e.preventDefault();
     const form = {
-      email: e.target[1].value,
-      password: e.target[2].value,
+      email: e.target[0].value,
+      password: e.target[1].value,
     };
 
-
+    console.log(form, form);
     console.log("submitted");
-  }
+  };
   const isError = false;
 
   return (
-   <Flex flexDirection='row' h='28.5em'  >
-    <Flex flexDirection='column' rowGap='10px' p='10px' w='50%' bg='royalBlue'>
-    <Text fontWeight='bold' fontSize='1.4rem' color='white' >Login</Text>
-    <Text color='white' fontSize='1.2rem'>
-Get access to your Orders, Wishlist and Recommendations</Text>
+    <Flex flexDirection="row" h="28.5em">
+      <Flex
+        flexDirection="column"
+        rowGap="10px"
+        p="10px"
+        w="50%"
+        bg="royalBlue"
+      >
+        <Text fontWeight="bold" fontSize="1.4rem" color="white">
+          Login
+        </Text>
+        <Text color="white" fontSize="1.2rem">
+          Get access to your Orders, Wishlist and Recommendations
+        </Text>
+      </Flex>
+      <form onSubmit={handleRegisterForm}>
+        <Text
+          color="royalBlue"
+          fontWeight="medium"
+          fontSize="1.6rem"
+          mb="30px"
+          textAlign="center"
+        >
+          Login
+        </Text>
+        <FormControl isRequired isInvalid={isError}>
+          <FormLabel>Email</FormLabel>
 
-      
+          <Input type="email" placeholder="abc@xyz.com" />
+        </FormControl>
+
+        <FormControl isRequired>
+          <FormLabel>Password</FormLabel>
+
+          <Input placeholder="password" />
+        </FormControl>
+        <Button
+          align="center"
+          mt={4}
+          bg="#fb641b"
+          color="white"
+          // isLoading={props.isSubmitting}
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
     </Flex>
-     <form onSubmit={handleRegisterForm}>
-     <Text color='royalBlue' fontWeight='medium' fontSize='1.6rem' mb='30px' textAlign='center'>Login</Text>
-      <FormControl isRequired isInvalid={isError}>
-        <Input type="email" placeholder='abc@xyz.com' />
-        {!isError ? (
-          <FormHelperText>
-            Enter the email you'd like to receive the newsletter on.
-          </FormHelperText>
-        ) : (
-          <FormErrorMessage>Email is required.</FormErrorMessage>
-        )}
-      </FormControl>
-
-      <FormControl isRequired>
-        <Input placeholder="password" />
-      </FormControl>
-      <Button
-      align='center'
-            mt={4}
-            bg='#fb641b'
-            color='white'
-            // isLoading={props.isSubmitting}
-            type='submit'
-          >
-            Submit
-          </Button>
-    </form>
-   </Flex>
   );
 }
-
